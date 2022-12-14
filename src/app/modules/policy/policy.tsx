5a79679d5ea0bf230e9f0f3b2ad30499cc2fd3f6
XFJS0006 <XFJS0006@corphq.hk.pccw.com>
2022-12-14 11:14:43 +0700

    add declaration and policy

--- src/app/modules/policy/policy.tsx
+++ src/app/modules/policy/policy.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import './policy.scss';
+import { Translate } from 'react-jhipster';
 
 const Policy = () => {
-  return <div>This is Policy</div>;
+  return (
+    <>
+      <div className="row policy-banner">
+        <div className="col-md-12 policy-banner-content">
+          <hr className={'line-separate'} />
+          <br />
+          <div className="row form-pd">
+            <div className="col-md-auto col-2">
+              <span className={'policy-step'}>D</span>
+            </div>
+            <div className={'col-md-auto col-10'}>
+              <Translate contentKey={'policy.banner'} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row well policy-container">
+        <div className="col-xs-12 policy-content txt-en">
+          <span>
+            <Translate contentKey={'policy.content'} />
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Policy;
